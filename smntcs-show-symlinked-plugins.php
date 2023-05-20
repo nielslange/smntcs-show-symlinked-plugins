f4dfe38240945001b5b336bb0dd547d6b40c0d60
Niels Lange <info@nielslange.de>
2023-05-20 21:35:10 +0700

    Optimize caching and make version number dynamic

--- smntcs-show-symlinked-plugins.php
+++ smntcs-show-symlinked-plugins.php
@@ -24,14 +24,49 @@ class SMNTCS_Show_Symlinked_Plugins {
 	 *
 	 * @var string
 	 */
-	private $version = '1.0';
+	private $version;
+
+	/**
+	 * Returns the plugin version.
+	 *
+	 * @return string
+	 */
+	public function getVersion() {
+		return $this->version;
+	}
 
 	/**
 	 * SMNTCS_Show_Symlinked_Plugins constructor.
 	 */
 	public function __construct() {
+		$this->version = $this->get_plugin_version();
+
 		add_action( 'admin_footer', array( $this, 'add_custom_class_to_plugin_row' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_styles' ) );
+		add_action( 'activated_plugin', array( $this, 'flush_plugins_cache' ) );
+		add_action( 'deactivated_plugin', array( $this, 'flush_plugins_cache' ) );
+		add_action( 'delete_plugin', array( $this, 'flush_plugins_cache' ) );
+		add_action( 'switch_theme', array( $this, 'flush_plugins_cache' ) );
+		add_action( 'symlink_plugin', array( $this, 'flush_plugins_cache' ) );
+		add_action( 'uninstall_plugin', array( $this, 'flush_plugins_cache' ) );
+	}
+
+	/**
+	 * Returns the plugin version by parsing the plugin header.
+	 *
+	 * @return string
+	 */
+	private function get_plugin_version() {
+		static $version;
+
+		if ( isset( $version ) ) {
+			return $version;
+		}
+
+		$plugin_data = get_file_data( __FILE__, array( 'Version' => 'Version' ) );
+		$version     = $plugin_data['Version'];
+
+		return $version;
 	}
 
 	/**
@@ -43,34 +78,72 @@ class SMNTCS_Show_Symlinked_Plugins {
 			return;
 		}
 
-		$plugins = get_plugins();
-		foreach ( $plugins as $plugin_file => $plugin_data ) {
-			$real_path = realpath( WP_PLUGIN_DIR . '/' . $plugin_file );
-			if ( $real_path && dirname( $real_path ) !== WP_PLUGIN_DIR . '/' . dirname( $plugin_file ) ) {
-				?>
-				<script type="text/javascript">
-				jQuery(document).ready(function($) {
-					const row = $( `tr[data-plugin="<?php echo esc_html( $plugin_file ); ?>"]`);
+		$plugins = $this->get_cached_plugins();
 
-					// Adds the "symlinked" class to the plugin row.
-					row.addClass( 'symlinked' );
+		$symlinkedPlugins = array_filter($plugins,
+			function( $plugin_data, $plugin_file ) {
+				$real_path = realpath( WP_PLUGIN_DIR . '/' . $plugin_file );
+				return $real_path && dirname( $real_path ) !== WP_PLUGIN_DIR . '/' . dirname( $plugin_file );
+			},
+		ARRAY_FILTER_USE_BOTH);
 
-					// Adds the "Symlinked" text to the front of the actions row.
-					row.find( '.row-actions' ).prepend( '<span class="symlinked-text">Symlinked</span> | ' );
-
-					// Removes the delete button when plugin is not active.
-					row.find('.delete').remove();
-
-					// Removes the " | " separator behind the "Activate" link when plugin is active.
-					row.find('.activate').html(function(_, html){ return html.replace(' | ', ''); });
-
-					// Removes the "Enable auto-updates" link.
-					row.find( '.toggle-auto-update' ).remove();
-				});
-				</script>
-				<?php
-			}
+		if ( empty( $symlinkedPlugins ) ) {
+			return;
 		}
+
+		ob_start();
+		?>
+	<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		<?php foreach ( $symlinkedPlugins as $plugin_file => $plugin_data ) : ?>
+			const row<?php echo esc_js( $plugin_file ); ?> = $('tr[data-plugin="<?php echo esc_attr( $plugin_file ); ?>"]');
+
+			// Adds the "symlinked" class to the plugin row.
+			row<?php echo esc_js( $plugin_file ); ?>.addClass('symlinked');
+
+			// Adds the "Symlinked" text to the front of the actions row.
+			row<?php echo esc_js( $plugin_file ); ?>.find('.row-actions').prepend('<span class="symlinked-text">Symlinked</span> | ');
+
+			// Removes the delete button when the plugin is not active.
+			row<?php echo esc_js( $plugin_file ); ?>.find('.delete').remove();
+
+			// Removes the " | " separator behind the "Activate" link when the plugin is active.
+			row<?php echo esc_js( $plugin_file ); ?>.find('.activate').html(function(_, html) {
+				return html.replace(' | ', '');
+			});
+
+			// Removes the "Enable auto-updates" link.
+			row<?php echo esc_js( $plugin_file ); ?>.find('.toggle-auto-update').remove();
+		<?php endforeach; ?>
+	});
+	</script>
+		<?php
+		ob_end_flush();
+	}
+
+	/**
+	 * Retrieves the list of plugins from cache or database.
+	 *
+	 * @return array
+	 */
+	private function get_cached_plugins() {
+		$cache_key = 'smntcs_plugins_cache';
+		$plugins   = get_transient( $cache_key );
+
+		if ( false === $plugins ) {
+			$plugins = get_plugins();
+			set_transient( $cache_key, $plugins, DAY_IN_SECONDS );
+		}
+
+		return $plugins;
+	}
+
+	/**
+	 * Flushes the plugins cache.
+	 */
+	public function flush_plugins_cache() {
+		$cache_key = 'smntcs_plugins_cache';
+		delete_transient( $cache_key );
 	}
 
 	/**
